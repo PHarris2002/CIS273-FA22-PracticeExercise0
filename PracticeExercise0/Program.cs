8cbe7b10d7267c4019b3fa5035eee861c2b5d0f4
Priscilla Harris <89226780+PHarris2002@users.noreply.github.com>
2022-09-01 22:55:05 -0500

    Neil Numbers Progress 9/1 10:54pm

--- PracticeExercise0/Program.cs
+++ PracticeExercise0/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace PracticeExcercise0
 {
@@ -25,19 +26,43 @@ namespace PracticeExcercise0
             IsUnique("Robbie");
             IsUnique("Judah");
 
+            Console.WriteLine(IsNeilNumber(99999));
+
         }
 
 
         // #1
         public static bool IsPalindrome(string s)
         {
+            // clean up the string; remove spaces and lowercase all capital letters
+            // reverse the clean string
+            string cleanString = s.ToLower().Replace(" ", "");
+
+            // check if reverse string is the same as the clean string
+            string reversed = Reverse(cleanString);
+
+            // check if reverse string is the same as the clean string
+            if (reversed == cleanString)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
             return true;
         }
 
         private static string Reverse(string s)
         {
             string result = "";
-            
+
+            foreach (char c in s)
+            {
+                result = result.Insert(0, c.ToString());
+            }
+
 
             return result;
         }
@@ -46,11 +71,43 @@ namespace PracticeExcercise0
         public static int ReverseInt(int i)
         {
             return 0;
-        }
+            // 369 % 10 = 9
+            // 369 / 10 = 36
+            // 36 % 10 = 6
+            // 36 / 10 = 3
+            // 3 % 10 = 3
+            // 3 / 10 = 0
+
+            int x = i;
+            int reverse = 0;
+
+            while (x != 0 )
+            {
+                int remainder = x % 10;
+                reverse = reverse * 10 + remainder;
+                x = x / 10;
+            }
+            return reverse;
+         }
 
         // #3
         public static bool IsUnique(string s)
         {
+            string cleanString = s.ToLower().Replace(" ", "");
+
+            for (int i=0; i < cleanString.Length; i++)
+            {
+                // compare cleanString[i] to every subsequent letter
+                // in cleanString
+
+                for(int j=i+1; j < cleanString.Length; j++)
+                {
+                    if (cleanString[i] == cleanString[j])
+                    {
+                        return false;
+                    }
+                }
+            }
             
             return true;
         }
@@ -59,7 +116,41 @@ namespace PracticeExcercise0
         // #4
         public static bool IsNeilNumber(int i)
         {
-            return true;
+
+            //counts digits so the digitCount can be used as an exponent
+            int digitCount = 0;
+
+            //makes input number iterable
+            string stringNumber = i.ToString();
+
+            //counter
+            foreach (char number in stringNumber)
+            {
+                digitCount += 1;
+            }
+
+            //gets sum of each number powered by the number of digits in number input
+            int poweredSum = 0;
+
+            foreach (char number in stringNumber)
+            {
+                string convertedNumber = number.ToString();
+                int poweredNumber = (int)Math.Pow(int.Parse(convertedNumber), digitCount);
+                poweredSum += poweredNumber;
+                Console.WriteLine(poweredSum);
+
+            }
+
+            if (i == poweredSum)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+            
         }
 
     }
